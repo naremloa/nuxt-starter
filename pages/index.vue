5f2bc7ce9c62aa684d95960c6f9cbff254493b22
naremloa <naremloa@gmail.com>
2025-06-06 12:45:48 +0800

    tmp

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const router = useRouter()
+const route = useRoute()
+
+const { data } = useAsyncData(
+  'data',
+  () => $fetch('https://jsonplaceholder.typicode.com/posts/1'),
+)
+
+function onClick() {
+  router.replace({ query: { a: 1 } })
+}
+</script>
+
+<template>
+  <div>
+    <div>{{ route.query }}</div>
+    <pre>{{ data }}</pre>
+    <button @click="onClick">
+      asdfsdf
+    </button>
+  </div>
+</template>
